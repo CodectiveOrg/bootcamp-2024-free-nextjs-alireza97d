2d25a49d74032c6d8702d34b3f8801ce083a1409
Alireza Dorrani <alireza97d@gmail.com>
2024-12-23 16:13:36 +0330

    [REFACTOR] Refactor Footer component and extract menu data to constants
    - Extracted social media links and certificates into separate constant files for better maintainability.
    - Replaced hardcoded menu items in Footer with dynamic mapping from the constants.
    - Improved code organization and readability.
    - Used TypeScript types for better type safety in menu items.

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -3,13 +3,7 @@ import { ReactElement } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import certificateLogo from "@/assets/logo/certificate.svg";
-import enamadLogo from "@/assets/logo/enamad.svg";
-import idkLogo from "@/assets/logo/idk.svg";
-
-import MingcuteLinkedinFill from "@/icons/MingcuteLinkedinFill";
-import MingcuteTelegramFill from "@/icons/MingcuteTelegramFill";
-import MingcuteYoutubeFill from "@/icons/MingcuteYoutubeFill";
+import { certificatesMenu, certificatesMenuType, socialsMenu, socialsMenuType } from "@/constant/footer/menu";
 
 import styles from "./footer.module.css";
 
@@ -19,57 +13,33 @@ export default function FooterComponent(): ReactElement {
             <div className={styles.writings}>
                 <div className={styles.logo}>دکتر من</div>
                 <p className={styles.description}>
-                    تجربه مشاوره آنلاین و دریافت نوبت از بهترین پزشکان و بیمارستان‌های
-                    ایران
+                    تجربه مشاوره آنلاین و دریافت نوبت از بهترین پزشکان و بیمارستان‌های ایران
                 </p>
             </div>
             <div className={styles.visuals}>
                 <ul className={styles.certificates}>
-                    <li>
-                        <Link href="#">
-                            <Image src={idkLogo} alt="IDK Logo" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="#">
-                            <Image src={certificateLogo} alt="Certificate Logo" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="#">
-                            <Image src={enamadLogo} alt="Enamad Logo" />
-                        </Link>
-                    </li>
+                    {certificatesMenu.map((item: certificatesMenuType, index: number) => (
+                        <li key={`certificates-${index}`}>
+                            <Link href={item.href}>
+                                <Image src={item.svgSrc} alt={item.alt} />
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
                 <ul className={styles.socials}>
-                    <li>
-                        <Link href="https://t.me/Codective" target="_blank">
-                            <MingcuteTelegramFill />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="https://www.linkedin.com/in/bijanprogrammer/"
-                            target="_blank"
-                        >
-                            <MingcuteLinkedinFill />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="https://www.youtube.com/@BijanProgrammer"
-                            target="_blank"
-                        >
-                            <MingcuteYoutubeFill />
-                        </Link>
-                    </li>
+                    {socialsMenu.map((item: socialsMenuType, index: number) => (
+                        <li key={`socials-${index}`}>
+                            <Link href={item.href} target="_blank" aria-label={item.alt}>
+                                <item.imgComponent />
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </div>
             <p className={styles.copy}>
                 تمامی حقوق مادی و معنوی این وب‌سایت، خدمات و محتوای مربوط به آن متعلق به
-                &nbsp;<strong>سامانه دکتر من</strong>&nbsp;
-                می‌باشد!
+                &nbsp;<strong>سامانه دکتر من</strong>&nbsp; می‌باشد!
             </p>
         </footer>
     );
-}
+};
