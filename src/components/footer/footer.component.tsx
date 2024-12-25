58187978d9561cba4fa67119f03d9af2ef9d90e1
Alireza Dorrani <63502585+alireza97d@users.noreply.github.com>
2024-12-25 20:57:23 +0330

    Lesson 2 (#3)
    * [Add] prettier
    
    * [CI/CD][ADD] lint.yml
    
    * [ADD] postcss nesting
    
    * [ADD] css reset
    
    * [ADD] css variables
    
    * [ADD] dark mode
    
    * [REMOVE] unnecessary code and files
    
    * [UPDATE] favicon
    
    * [ADD] footer
    
    * [UPDATE] home page
    
    * [REFACTOR]
    Refactor Footer component and extract menu data to constants
    
    - Extracted social media links and certificates into separate constant files for better maintainability.
    - Replaced hardcoded menu items in Footer with dynamic mapping from the constants.
    - Improved code organization and readability.
    - Used TypeScript types for better type safety in menu items.
    
    * [UPDATE] fixed input for responsive home page
    
    * [PRETTIER] fixing format

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,51 @@
+import { ReactElement } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import {
+  certificatesMenu,
+  certificatesMenuType,
+  socialsMenu,
+  socialsMenuType,
+} from "@/constant/footer/menu";
+
+import styles from "./footer.module.css";
+
+export default function FooterComponent(): ReactElement {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.writings}>
+        <div className={styles.logo}>دکتر من</div>
+        <p className={styles.description}>
+          تجربه مشاوره آنلاین و دریافت نوبت از بهترین پزشکان و بیمارستان‌های
+          ایران
+        </p>
+      </div>
+      <div className={styles.visuals}>
+        <ul className={styles.certificates}>
+          {certificatesMenu.map((item: certificatesMenuType, index: number) => (
+            <li key={`certificates-${index}`}>
+              <Link href={item.href}>
+                <Image src={item.svgSrc} alt={item.alt} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <ul className={styles.socials}>
+          {socialsMenu.map((item: socialsMenuType, index: number) => (
+            <li key={`socials-${index}`}>
+              <Link href={item.href} target="_blank" aria-label={item.alt}>
+                <item.imgComponent />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <p className={styles.copy}>
+        تمامی حقوق مادی و معنوی این وب‌سایت، خدمات و محتوای مربوط به آن متعلق به
+        &nbsp;<strong>سامانه دکتر من</strong>&nbsp; می‌باشد!
+      </p>
+    </footer>
+  );
+}
---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { ReactElement } from "react";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import clsx from "clsx";
+
+import { menu, menuType } from "@/constant/header/menu";
+import styles from "./header.module.css";
+
+export default function HeaderComponent(): ReactElement {
+  const pathname = usePathname();
+
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          {menu.map((item: menuType, index: number) => {
+            const title = item.title;
+            const href = item.href;
+            return (
+              <li key={index}>
+                <Link
+                  href={`/${href}`}
+                  className={clsx(pathname === `/${href}` && styles.active)}
+                >
+                  {title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <button className={styles.cta}>ورود | ثبت‌نام</button>
+    </header>
+  );
+}
