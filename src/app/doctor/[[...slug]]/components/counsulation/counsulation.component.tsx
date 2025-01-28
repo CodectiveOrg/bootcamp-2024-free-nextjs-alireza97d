fd6074473349d1e5fb8eb36d2ad7de472b3cdfc2
Alireza Dorrani <alireza97d@gmail.com>
2025-01-28 21:41:54 +0330

    [ADD] online counsulation component

--- src/app/doctor/[[...slug]]/components/counsulation/counsulation.component.tsx
+++ src/app/doctor/[[...slug]]/components/counsulation/counsulation.component.tsx
@@ -3,11 +3,45 @@ import { ReactElement } from "react";
 import CardComponent from "@/components/card/card.component";
 
 import styles from "./counsulation.module.css"
+import { MingcuteArrowLeftLine } from "@/icons/MingcuteArrowLeftLine";
+
+const price = 190000;
 
 const CounsulationComponent = (): ReactElement => {
     return (
-        <CardComponent>
-            <div className=''>CounsulationComponent</div>
+        <CardComponent customStyle={styles.card}>
+            <div className={styles.container}>
+                <div className={styles.price}>
+                    <span>همین الان آنلاین ویزیت شوید</span>
+                    <span>{price.toLocaleString()} تومان</span>
+                </div>
+
+                <div className={styles.note}>
+                    <ul>
+                        <li>ویزیت آنلاین در پیام رسان:</li>
+                        <li>تضمین بازپرداخت مبلغ ویزیت در صورت نارضایتی</li>
+                        <li>امکان برقراری تماس با این پزشک وجود دارد.</li>
+                        <li>
+                            تا
+                            &nbsp;
+                            <span>۳ روز</span>
+                            &nbsp;
+                            می‌توانید هر سوالی دارید از پزشک بپرسید
+                        </li>
+                        <li>میانگین زمان انتظار تا ویزیت:</li>
+                    </ul>
+                </div>
+
+                <button>
+                    <span>
+                        <span>شروع ویزیت با دکتر</span>
+                        &nbsp;
+                        <span>پوراندخت جعفری</span>
+                    </span>
+
+                    <MingcuteArrowLeftLine />
+                </button>
+            </div>
         </CardComponent>
     )
 }
