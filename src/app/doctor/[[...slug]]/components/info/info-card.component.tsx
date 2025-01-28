4a708e48ee59b4bb633328a320002416f4fcd900
Alireza Dorrani <alireza97d@gmail.com>
2025-01-28 20:34:01 +0330

    [ADD] doctor info card

--- src/app/doctor/[[...slug]]/components/info/info-card.component.tsx
+++ src/app/doctor/[[...slug]]/components/info/info-card.component.tsx
@@ -11,8 +11,44 @@ type Props = { doctorDetails: DoctorModel };
 
 const InfoCardComponent: React.FC<Props> = ({ doctorDetails }): ReactElement => {
     return (
-        <div className={styles.info}>
-            <DoctorImageComponent {...doctorDetails} />
+        <div className={styles.card}>
+
+            <div className={styles.action}>
+                <ul>
+                    <li>
+                        <button>ذخیره</button>
+                    </li>
+                    <li>
+                        <button>اشتراک گذاری</button>
+                    </li>
+                    <li>
+                        <button>گزارش</button>
+                    </li>
+                </ul>
+
+                <span>10K</span>
+            </div>
+
+            <div className={styles.info}>
+                <DoctorImageComponent {...doctorDetails} />
+
+                <div className="">
+                    <h1>{doctorDetails.name}</h1>
+                    <span>شماره نظام پزشکی: 165017</span>
+                </div>
+            </div>
+
+            <div className={styles.brief_container}>
+                <strong className={styles.brief}>
+                    {doctorDetails.brief}
+                </strong>
+
+                <span className={styles.rate}>
+                    <span className={styles.ave_rate}>{doctorDetails.averageRating} از 5</span>
+                    &nbsp;رضایت
+                    ({doctorDetails.totalVotes} نظر)
+                </span>
+            </div>
         </div>
     )
 }
