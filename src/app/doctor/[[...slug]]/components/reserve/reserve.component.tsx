59e9c344f292919bed06fbd06f987d2c39ca8a76
Alireza Dorrani <alireza97d@gmail.com>
2025-01-28 21:47:28 +0330

    [ADD] reserve component

--- src/app/doctor/[[...slug]]/components/reserve/reserve.component.tsx
+++ src/app/doctor/[[...slug]]/components/reserve/reserve.component.tsx
@@ -2,14 +2,28 @@
 import { ReactNode } from "react";
 
 import CardComponent from "@/components/card/card.component";
+import { MingcuteArrowLeftLine } from "@/icons/MingcuteArrowLeftLine";
 
 import styles from "./reserve.module.css";
 
 const ReserveComponent = (): ReactNode => {
     return (
-        <CardComponent>
-            <div className="">
-                ReserveComponent
+        <CardComponent customStyle={styles.card}>
+            <div className={styles.container}>
+                <div className={styles.price}>
+                    <span>نوبت اینترنتی و مراجعه حضوری</span>
+                </div>
+
+                <div className={styles.note}>
+                    <ul>
+                        <li>امکان دریافت زودترین نوبت</li>
+                    </ul>
+                </div>
+
+                <button>
+                    <span>دریافت نوبت</span>
+                    <MingcuteArrowLeftLine />
+                </button>
             </div>
         </CardComponent>
     )
