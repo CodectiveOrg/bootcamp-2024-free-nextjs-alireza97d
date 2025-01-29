c9b95c4deaefcc7d1805fcb0b63827dab68c275d
Alireza Dorrani <alireza97d@gmail.com>
2025-01-29 15:44:10 +0330

    [ADD] reating progress bar

--- src/app/doctor/[[...slug]]/components/reviews/reviews.component.tsx
+++ src/app/doctor/[[...slug]]/components/reviews/reviews.component.tsx
@@ -1,17 +1,32 @@
-
 import { ReactNode } from "react";
 
 import CardComponent from "@/components/card/card.component";
+import RatingProgressComponent from "./rating-progress.component";
 
 import styles from "./reviews.module.css";
 
 const ReviewsComponent = (): ReactNode => {
     return (
-        <CardComponent>
-            <div className="">
-                ReviewsComponent
+        <div className={styles.container}>
+            <div className={styles.title}>
+                <h2>
+                    <span>نظرات در مورد دکتر</span>
+                    &nbsp;
+                    <span>معصومه دلجو</span>
+                </h2>
             </div>
-        </CardComponent>
+            <CardComponent>
+                <div className={styles.rate}>
+                    <span className={styles.ave_rate}>{2} از 5</span>
+                    &nbsp;رضایت
+                    ({264} نظر)
+                </div>
+
+                <div className={styles.review}>
+                    <RatingProgressComponent />
+                </div>
+            </CardComponent>
+        </div>
     )
 }
 
