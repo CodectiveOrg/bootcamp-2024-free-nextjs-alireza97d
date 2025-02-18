0704568984ac663d239b469540914b0de6f3cb09
Alireza Dorrani <alireza97d@gmail.com>
2025-02-18 23:07:57 +0330

    [UPDATE] replace google font with locale font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 
 import type { Metadata } from "next";
-import { Vazirmatn } from "next/font/google";
+import localFont from "next/font/local";
 
 import FooterComponent from "@/components/footer/footer.component";
 import HeaderComponent from "@/components/header/header.component";
@@ -9,16 +9,61 @@ import HeaderComponent from "@/components/header/header.component";
 import "@/styles/typography.css";
 import "./globals.css";
 
-const vazirmatn = Vazirmatn({
-  subsets: ["latin", "arabic"],
-  display: "swap",
-  variable: "--vazir-font",
-});
-
 export const metadata: Metadata = {
   title: "دکتر من",
   description: "پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین",
 };
+
+const vazirmatn_FD = localFont({
+  src: [
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-Thin.woff2",
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-ExtraLight.woff2",
+      weight: '200',
+      style: 'normal'
+    },
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-Light.woff2",
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-Regular.woff2",
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-Medium.woff2",
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-SemiBold.woff2",
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-Bold.woff2",
+      weight: '700',
+      style: 'normal'
+    },
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-ExtraBold.woff2",
+      weight: '800',
+      style: 'normal'
+    },
+    {
+      path: "../public/fonts/vazirmatn/Vazirmatn-FD-Black.woff2",
+      weight: '900',
+      style: 'normal'
+    },
+  ],
+  variable: "--font-vazirmatn"
+});
 
 export default function RootLayout({
   children,
@@ -26,7 +71,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>): ReactElement {
   return (
-    <html lang="fa" dir="rtl" className={vazirmatn.variable}>
+    <html lang="fa" dir="rtl" className={vazirmatn_FD.variable}>
       <body>
         <HeaderComponent />
         <main>{children}</main>
