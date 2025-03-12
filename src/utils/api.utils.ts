3c982d5dfa6b3a4f80cac6846daad3cd787feb33
Alireza Dorrani <alireza97d@gmail.com>
2025-03-12 23:24:32 +0330

    [ADD] sign up api route

---
+++ src/utils/api.utils.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+import { ApiResponseType } from "@/types/api.response";
+
+export async function wrapWithTryCatch<T>(
+    callback: () => Promise<ApiResponseType<T>>,
+): Promise<ApiResponseType<T>> {
+    try {
+        return await callback()
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message }, { status: 400 });
+        };
+
+        return NextResponse.json({ error: "خطای غیرمنتظره رخ داده." }, { status: 500 });
+    }
+}
