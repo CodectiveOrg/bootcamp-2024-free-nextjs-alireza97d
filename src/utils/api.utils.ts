cca5822f596b0c33d02f8840eb9d88e0b231b651
Alireza Dorrani <alireza97d@gmail.com>
2025-03-12 23:46:06 +0330

    [ADD] parseBody

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -2,6 +2,30 @@ import { NextResponse } from "next/server";
 
 import { ApiResponseType } from "@/types/api.response";
 
+type ParseBodyResult<T> = [error: null, data: T] | [error: string, data: null];
+
+export async function parseBody<T>(
+    request: Request,
+): Promise<ParseBodyResult<T>> {
+    try {
+        const body = await request.json();
+        return [null, body];
+    } catch (error) {
+        if (error instanceof Error) {
+            // return [error.message, null];
+            if (error.name === "SyntaxError") {
+                return ["فرمت body نادرست است.", null]
+            }
+        }
+
+        if (typeof error === "string") {
+            return [error, null];
+        }
+
+        return ["خطای غیر منتظره رخ داده.", null];
+    }
+}
+
 export async function wrapWithTryCatch<T>(
     callback: () => Promise<ApiResponseType<T>>,
 ): Promise<ApiResponseType<T>> {
